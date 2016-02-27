35cc1a424c5923ab163e5a82bf0af873420fa266
Ramnarayan Vedam <ramnarayan.vedam@varian.com>
2016-02-26 21:08:57 -0500

    Added stock and stock portfolio objects and fixed errors

--- js/stock.js
+++ js/stock.js
@@ -1,19 +1,32 @@
 'use strict';
 
 class Stock {
-  String symbol;
-  String name;
-  Number currentPrice;
-  Number daysHigh;
-  Number yearsHigh;
-  Number yearsLow;
-
-  constructor() {
-    updateInfo();
+  constructor(symbol) {
+    this.symbol = symbol;
+    this.name = '';
+    this.currentPrice = 0.00;
+    this.daysHigh   = 0.00;
+    this.yearsHigh  = 0.00;
+    this.yearsLow   = 0.00;
   }
 
   updateInfo() {
-    return new Promise({
-    });
+    let xhr = new XMLHttpRequest();
+    let query = 'select * from yahoo.finance.quotes where symbol in (" ${this.symbol} ")';
+    xhr.url = 'http://query.yahooapis.com/v1/public/yql?q=${query}';
+    xhr.url = xhr.url + '&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
+    xhr.onreadystatechange = function (data) {
+      let stockData = JSON.parse(data);
+      if(stockData) {
+        this.name = stockData['query']['results']['quote']['Name'];
+        this.currentPrice = stockData['query']['results']['quote']['Ask'];
+        this.daysHigh = stockData['query']['results']['quote']['DaysHigh'];
+        this.daysLow = stockData['query']['results']['quote']['DaysLow'];
+        this.yearsHigh = stockData['query']['results']['quote']['YearHigh'];
+        this.yearsLow = stockData['query']['results']['quote']['YearLow'];
+      }
+    };
+    xhr.send();
+    return xhr;
   }
 }
--- js/stock_portfolio.js
+++ js/stock_portfolio.js
@@ -2,5 +2,13 @@
 
 class StockPortfolio {
   constructor() {
+    this.stocks = [];
+    this.name = '';
+    this.description = '';
+  }
+
+  update() {
+    stocks.forEach(stock => stock.updateInfo());
   }
 }
+
